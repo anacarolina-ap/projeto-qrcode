0ed87bcdd34a3945f8392844161bc00bfd90dea8
anacarolina-ap <anacarolina-alm@hotmail.com>
2024-12-26 20:37:00 -0300

    Projeto com gerador de Password e QRCode

---
+++ src/services/password/create.js
@@ -0,0 +1,10 @@
+import handlePassword from "./handle.js";
+import chalk from "chalk";
+
+async function createPassword() {
+    console.log(chalk.green("password"));    
+    const password = await handlePassword(); 
+    console.log(password)
+}
+
+export default createPassword;
---
+++ src/services/qr-code/handle.js
@@ -0,0 +1,19 @@
+
+import chalk from "chalk";
+import qr from "qrcode-terminal"
+
+async function handleQRCode(err, result) {
+    if (err) {
+        console.log("error on application")
+        return;
+    }
+
+    const isSmall = result.type == 2;
+    qr.generate(result.link, {small: isSmall}, (qrcode) => {
+        console.log(chalk.green("QRCode gerado com sucesso! \n"))
+        console.log(qrcode)
+    })
+      
+}
+
+export default handleQRCode;
